8e7af36d33645bbd066b3bfcdbe733546a7c4297
Matheus Henrique C Barbosa <matheus.henrique.campos0408@gmail.com>
2025-03-28 17:21:15 -0300

    Add files via upload

---
+++ Ativ_10.java
@@ -0,0 +1,53 @@
+package try_catch;
+
+import java.util.Scanner;
+
+public class Ativ_10 {
+
+    //classe para chamada recursiva
+    public static long factorial(int n ) {
+        if (n != 0) {
+            return n * factorial(n - 1);
+        }else{
+            return 1;
+        }
+}
+
+    public static class NumeroNegativoException extends Exception{
+        public NumeroNegativoException(String messageNegative) {
+            super(messageNegative);
+        }
+    }
+
+    public static class LimiteFatorialExcedidoException extends Exception{
+        public LimiteFatorialExcedidoException(String messageLimit) {
+            super(messageLimit);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("-------------------Calculadora de Fatorial-------------------");
+            try {
+                System.out.print("Insira um número: ");
+                Integer num=sc.nextInt();
+
+                if (num<0){
+                    throw new NumeroNegativoException("Entrada inválida!");
+                }
+                if (num>20){
+                    throw new LimiteFatorialExcedidoException("Entrada excedida!");
+                }
+
+                long result = factorial(num);
+                System.out.println(num+"! ="+result);
+            }catch (NumeroNegativoException e){
+                System.out.println(e.getMessage());
+            }catch (LimiteFatorialExcedidoException e){
+                System.out.println(e.getMessage());
+            }finally {
+                sc.close();
+            }
+    }
+}
